1b26762a177e1545a8662a3a8dd9bc840175b2e9
Nicolas Pla <nicolas.pla@theappmaster.com>
2026-09-01 15:51:25 -0300

    fix: lock app orientation to portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
@@ -7,6 +8,9 @@ import 'presentation/search/search_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations(<DeviceOrientation>[
+    DeviceOrientation.portraitUp,
+  ]);
   await Hive.initFlutter();
   await setupServiceLocator();
   runApp(const ProviderScope(child: GapsiApp()));
